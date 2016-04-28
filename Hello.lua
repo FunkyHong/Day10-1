f93cdede2f58064b7479d132ba80d1c77e5cc6d3
FunkyHong <524851560@qq.com>
2016-04-28 11:38:01 +0800

    Funky

---
+++ Hello.lua
@@ -0,0 +1,15 @@
+-- myName = "beautiful girl"
+
+-- helloTable = {
+-- 	name = "zhangsan",
+-- 	IQ = 125
+-- }
+
+-- function helloAdd( num1,num2 )
+-- 	return (num1 + num2)
+-- end
+
+local num1,num2,num3 = cpp_GetNumber(10,20)
+print("lua_num1 = " .. num1)
+print("lua_num2 = " .. num2)
+print("lua_num3 = " .. num3)
